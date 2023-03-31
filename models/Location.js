1f3f75c1e7ffbc2bd0fc752201d38c8cde2d07f6
beryln-t <berylntee@gmail.com>
2023-03-31 18:52:45 +0800

    Update location model field naming

--- models/Location.js
+++ models/Location.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const locationSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  product: [
+  products: [
     {
-      productDetails: { type: Schema.Types.ObjectId, ref: "product" },
+      productDetails: { type: Schema.Types.ObjectId, ref: "Product" },
       productQty: { type: Int, required: true },
     },
   ],
