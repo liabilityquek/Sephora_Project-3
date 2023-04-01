932a649c059a1a501c0452b70cf897552ce56139
beryln-t <berylntee@gmail.com>
2023-04-01 16:40:16 +0800

    Update Location model with timestamp

--- models/Location.js
+++ models/Location.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const locationSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  products: [
-    {
-      productDetails: { type: Schema.Types.ObjectId, ref: "Product" },
-      productQty: { type: Int, required: true },
-    },
-  ],
-});
+const locationSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    products: [
+      {
+        productDetails: { type: Schema.Types.ObjectId, ref: "Product" },
+        productQty: { type: Int, required: true },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Location = mongoose.model("Location", locationSchema);
 module.exports = Location;
