f7ec4cbcf8745882c8d6b0852c4deac7b2d593c3
beryln-t <berylntee@gmail.com>
2023-04-01 22:44:08 +0800

    Create front-end for inventory index

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import Home from "../components/Home";
 import { Routes, Route } from "react-router";
 import NavBar from "../components/NavBar";
-import AppointmentPage from "./Appoinments/AppointmentPage"
+import AppointmentPage from "./Appoinments/AppointmentPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import InventoryManagement from "./InventoryManagement/InventoryManagement";
 
 function App() {
-
   return (
     <main className="App">
-        <NavBar />
-        <Routes>
-          <Route path="/booking" element={<AppointmentPage/>} />
-        </Routes>
-        <Home />
-      </main>
-
-  )
+      <NavBar />
+      <Home />
+      <Routes>
+        <Route path="/booking" element={<AppointmentPage />} />
+        <Route path="/adminlocation" element={<InventoryManagement />} />
+      </Routes>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/InventoryManagement/InventoryManagement.jsx
@@ -0,0 +1,10 @@
+import InventoryTable from "./InventoryTable/InventoryTable";
+
+export default function () {
+  return (
+    <div>
+      Welcome to Inventory management
+      <InventoryTable></InventoryTable>
+    </div>
+  );
+}
