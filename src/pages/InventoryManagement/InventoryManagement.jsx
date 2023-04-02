effc399a000bd5cf5726fd87a30a63139d773987
beryln-t <berylntee@gmail.com>
2023-04-02 22:52:44 +0800

    Update InventoryManagement.jsx

--- src/pages/InventoryManagement/InventoryManagement.jsx
+++ src/pages/InventoryManagement/InventoryManagement.jsx
@@ -3,7 +3,6 @@ import InventoryTable from "./InventoryTable/InventoryTable";
 export default function () {
   return (
     <div>
-      Welcome to Inventory management
       <InventoryTable></InventoryTable>
     </div>
   );
