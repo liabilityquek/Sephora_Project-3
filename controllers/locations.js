3e98669880ba83035dee13f94ca39fe39f7de3d3
beryln-t <berylntee@gmail.com>
2023-04-03 09:39:13 +0800

    Delete controller

--- controllers/locations.js
+++ controllers/locations.js
@@ -10,6 +10,37 @@ const showLocation = async (req, res) => {
   }
 };
 
+// const delLocProduct = async (req, res) => {
+//   try {
+//     const locationId = req.params.locationId; // get the location id from the request parameters
+//     const productId = req.params.productId; // get the product id from the request parameters
+
+//     // Find the location by its id
+//     const location = await Location.findById(locationId);
+
+//     // Find the index of the product in the product array
+//     const productIndex = location.product.findIndex(
+//       (p) => p.productDetails.toString() === productId
+//     );
+
+//     // If the product exists in the array, remove it
+//     if (productIndex >= 0) {
+//       location.product.splice(productIndex, 1);
+//     } else {
+//       return res.status(404).json({ error: "Product not found in location" });
+//     }
+
+//     // Save the updated location
+//     const updatedLocation = await location.save();
+
+//     // Return the updated location
+//     res.status(200).json(updatedLocation);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// };
+
 module.exports = {
   showLocation,
+  // deleteLocProduct,
 };
