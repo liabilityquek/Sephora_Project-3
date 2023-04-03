02f240e4e8524264061ac2df9674999585da4acc
beryln-t <berylntee@gmail.com>
2023-04-03 10:37:58 +0800

    Update delete controller

--- controllers/locations.js
+++ controllers/locations.js
@@ -10,37 +10,37 @@ const showLocation = async (req, res) => {
   }
 };
 
-// const delLocProduct = async (req, res) => {
-//   try {
-//     const locationId = req.params.locationId; // get the location id from the request parameters
-//     const productId = req.params.productId; // get the product id from the request parameters
+const deleteLocProduct = async (req, res) => {
+  try {
+    const locationId = req.params.locationId; // get the location id from the request parameters
+    const productId = req.params.productId; // get the product id from the request parameters
 
-//     // Find the location by its id
-//     const location = await Location.findById(locationId);
+    // Find the location by its id
+    const location = await Location.findById(locationId);
 
-//     // Find the index of the product in the product array
-//     const productIndex = location.product.findIndex(
-//       (p) => p.productDetails.toString() === productId
-//     );
+    // Find the index of the product in the product array
+    const productIndex = location.products.findIndex(
+      (p) => p.productDetails.toString() === productId
+    );
 
-//     // If the product exists in the array, remove it
-//     if (productIndex >= 0) {
-//       location.product.splice(productIndex, 1);
-//     } else {
-//       return res.status(404).json({ error: "Product not found in location" });
-//     }
+    // If the product exists in the array, remove it
+    if (productIndex >= 0) {
+      location.products.splice(productIndex, 1);
+    } else {
+      return res.status(404).json({ error: "Product not found in location" });
+    }
 
-//     // Save the updated location
-//     const updatedLocation = await location.save();
+    // Save the updated location
+    const updatedLocation = await location.save();
 
-//     // Return the updated location
-//     res.status(200).json(updatedLocation);
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// };
+    // Return the updated location
+    res.status(200).json(updatedLocation);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = {
   showLocation,
-  // deleteLocProduct,
+  deleteLocProduct,
 };
--- routes/locations.js
+++ routes/locations.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const locationsCtrl = require("../controllers/locations");
 
 router.get("/", locationsCtrl.showLocation);
-// router.delete(
-//   "/locations/:locationId/products/:productId",
-//   locationsCtrl.deleteLocProduct
-// );
+router.delete(
+  "/locations/:locationId/products/:productId",
+  locationsCtrl.deleteLocProduct
+);
 
 module.exports = router;
