da5d1eab8a51fa7d1595815a6096782f731faf8e
beryln-t <berylntee@gmail.com>
2023-04-07 17:26:53 +0800

    Header nav bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,65 +1,81 @@
-import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 export default function Header() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLoginClick = () => {
-    setIsLoggedIn(true);
-  };
-
-  const handleLogoutClick = () => {
-    setIsLoggedIn(false);
-  };
+  const isSignedIn = false; // Replace with your logic for checking if the user is signed in
 
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav me-auto">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 align-items-center">
+            <li className="nav-item">
+              <Link className="nav-link border-end text-white" to="/my-account">
+                My Account
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle text-white"
+                href="#"
+                id="navbarDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Admin Tools
+              </a>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <li>
+                  <Link className="dropdown-item" to="/productpage">
+                    Product Portfolio
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/adminlocation">
+                    Inventory Management
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/staff-scheduling">
+                    Staff Scheduling
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <ul className="navbar-nav">
+            {isSignedIn ? (
               <li className="nav-item">
-                <button type="button" className="btn btn-link text-white">
-                  My Account
-                </button>
+                <Link className="nav-link text-white" to="/sign-out">
+                  Sign Out
+                </Link>
               </li>
-            </ul>
-            <ul className="navbar-nav">
+            ) : (
               <li className="nav-item">
-                <button
-                  type="button"
-                  className="btn btn-outline-light"
-                  onClick={isLoggedIn ? handleLogoutClick : handleLoginClick}
-                >
-                  {isLoggedIn ? "Sign Out" : "Sign In"}
-                </button>
+                <Link className="nav-link text-white" to="/sign-in">
+                  Sign In
+                </Link>
               </li>
-            </ul>
-          </div>
+            )}
+          </ul>
         </div>
-      </nav>
-      <div className="text-center mb-3">
-        <img src="https://i.imgur.com/lhaMADj.jpg" alt="Banner" />
       </div>
-      <div className="d-flex justify-content-center align-items-center">
-        <img
-          src="https://i.imgur.com/uoiaM7q.png"
-          alt="Logo"
-          className="logo"
-        />
-      </div>
-    </div>
+    </nav>
   );
 }
