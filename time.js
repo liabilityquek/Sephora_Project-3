1a78259250c9073fdb177acfc33e17f2e855aeca
fjrmario <114366692+fjrmario@users.noreply.github.com>
2023-04-10 15:00:20 +0800

    added changes to time.js

--- time.js
+++ time.js
@@ -1,16 +1,20 @@
-import moment from "moment";
-
 const generateTimeSlots = (start, end, intervalMins) => {
   const slots = [];
 
   for (let i = start * 60; i <= end * 60; i += intervalMins) {
-    const currentMoment = moment().startOf("day").add(i, "minutes");
-    const nextMoment = currentMoment.clone().add(intervalMins, "minutes");
+    const h = Math.floor(i / 60);
+    const m = i % 60;
 
-    if (i + intervalMins > end * 60) break;
+    const nextTotalMins = i + intervalMins;
+    const nextH = Math.floor(nextTotalMins / 60);
+    const nextM = nextTotalMins % 60;
+
+    if (nextH === end && nextM > 45) break;
 
     slots.push(
-      `${currentMoment.format("HH:mm")} - ${nextMoment.format("HH:mm")}`
+      `${String(h).padStart(2, "0")}:${String(m).padStart(2, "0")} - ${String(
+        nextH
+      ).padStart(2, "0")}:${String(nextM).padStart(2, "0")}`
     );
   }
 
@@ -18,5 +22,6 @@ const generateTimeSlots = (start, end, intervalMins) => {
 };
 
 const time = generateTimeSlots(8, 23, 60);
+
 
 export { time };
