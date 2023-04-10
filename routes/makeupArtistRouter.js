3dc79ea143587144bbb84b1accf4516cfc84f948
fjrmario <114366692+fjrmario@users.noreply.github.com>
2023-04-10 15:05:15 +0800

    added authentication for all the HRADMIN actions

--- routes/makeupArtistRouter.js
+++ routes/makeupArtistRouter.js
@@ -1,14 +1,15 @@
 const makeupArtist = require("../controllers/makeupAdminController");
 const express = require("express");
 const router = express.Router();
+const { isAuth } = require("../controllers/customerAuthController");
 
 
-router.get("/edit/:id", makeupArtist.show)
-router.post("/", makeupArtist.create);
-router.get("/:locationId", makeupArtist.findMakeupArtistByLocation);
-router.get("/admin/:makeupArtistId", makeupArtist.findAppointmentByMakeupArtistId);
-router.put("/:makeupArtistId", makeupArtist.updateMakeupArtist);
-router.delete("/:id", makeupArtist.deleteMakeupArtist)
+router.get("/edit/:id",isAuth, makeupArtist.show)
+router.post("/", isAuth, makeupArtist.create);
+router.get("/:locationId", isAuth, makeupArtist.findMakeupArtistByLocation);
+router.get("/admin/:makeupArtistId", isAuth, makeupArtist.findAppointmentByMakeupArtistId);
+router.put("/:makeupArtistId", isAuth, makeupArtist.updateMakeupArtist);
+router.delete("/:id",isAuth, makeupArtist.deleteMakeupArtist)
 
 
 
